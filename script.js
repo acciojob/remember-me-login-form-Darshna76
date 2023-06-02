82e0f2fba35759ce96b3bac3fe1f2a8d826ded04
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-06-02 17:32:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your JS code here. If required.
+const form = document.getElementById('loginForm');
+  const checkbox = document.getElementById('checkbox');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (checkbox.checked) {
+      localStorage.setItem('rememberMe', 'true');
+      localStorage.setItem('username', username);
+    } else {
+      localStorage.removeItem('rememberMe');
+      localStorage.removeItem('username');
+    }
+
+    alert('Logged in as ' + username);
+
+    // Check if there are saved details
+    const savedUsername = localStorage.getItem('username');
+    if (savedUsername) {
+      const existingButton = document.createElement('button');
+      existingButton.id = 'existing';
+      existingButton.textContent = 'Login as existing user';
+
+      existingButton.addEventListener('click', function() {
+        alert('Logged in as ' + savedUsername);
+      });
+
+      form.appendChild(existingButton);
+    }
+  });
+
+  const rememberMe = localStorage.getItem('rememberMe');
+  if (rememberMe === 'true') {
+    checkbox.checked = true;
+  }
